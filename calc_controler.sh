054ded0227459189bceabdfe7a1ab5a30114b828
Sabry Razick <sabryr@gmail.com>
2021-10-31 14:39:12 +0100

    Add comments to controler

--- calc_controler.sh
+++ calc_controler.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# Controler script that starta
+# 3 jobs that last 3,2,1 secounds
+# each
 
-./calc_do.sh "A" 3 
-./calc_do.sh "B" 2 
-./calc_do.sh "C" 1 
+./calc_do.sh "A" 3 & 
+./calc_do.sh "B" 2 & 
+./calc_do.sh "C" 1 & 
