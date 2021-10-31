65454300499602c97b8311db213725a4e4bc9bb3
Sabry Razick <sabryr@gmail.com>
2021-10-31 14:36:03 +0100

    add intro

--- calc.sh
+++ calc.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+# Do 5 time, Three jobs that takes
+# 3,2 and 1 secounds each
+
 A=3
 B=2
 C=1
 
-for i in {1..10}
+for i in {1..5}
 do
   printf "A starting "$(date +%M:%S)
   sleep ${A}
-  #printf "A done "$(date +%M:%S)
   printf "\t B starting "$(date +%M:%S)
   sleep ${B}
-  #printf "\t B done "$(date +%M:%S)
   printf "\t\t C starting "$(date +%M:%S)
   sleep ${C}
-  #printf "\t\t C done "$(date +%M:%S)
   echo ".."
 done
